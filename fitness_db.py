be4bd38b4f2c4b16e1e09083b245de4599881bb9
Chris Daffron <cdaffron@vols.utk.edu>
2014-11-06 19:31:24 -0500

    Updated for 8 threads

--- fitness_db.py
+++ fitness_db.py
@@ -77,8 +77,18 @@ class FitnessDatabase:
 
   def get_unique_user_to_add(self, thread_id, user_array):
     if( thread_id == 0 ):
-      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } } ] } )
-    if( thread_id == 1 ):
-      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[2] } } ] } )
-    if( thread_id == 2 ):
-      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } } ] } )
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 1 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 2 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 3 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 4 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 5 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 6 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[7] } } ] } )
+    elif( thread_id == 7 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[6] } } ] } )
--- threaded_users.py
+++ threaded_users.py
@@ -55,13 +55,28 @@ CAMILLE_SECRET = 'fDsRmHDRmuAYUmqCHzRu4pqTrfNSDUfzk3BEK8c9cph'
 JOSH_ID = 'jjd8nkcx3sjr6weurtjqun4hp6b5p7vk'
 JOSH_SECRET = 'NNHSen7PqBXvyFneKvq4DvDetng6dU8kKZe8pYbx8E9'
 
+DAVID_ID = 'cv8rzqn82xq9uub7ybta4y69jxfyfmjb'
+DAVID_SECRET = 'WMbJFvksTUMgHFDTaCNtA9CRHmc8P6RhSUvXtF6cfny'
+
+AUDRIS_ID = 'smyfv7ec67q7t6jqcfwhrv299x4g4rtv'
+AUDRIS_SECRET = 'YGQEzsq877fhjZEEHR79F87EwjCn4ABncYTdvVfQGU8'
+
+JOHN_ID = 'cx64t5vm27e2pzgwqaqvs2fsxhsrhd73'
+JOHN_SECRET = 'yut7ya9PpYUkeDTtgRcdCH87Ee4eMXGXA5MK32GtWx5'
+
+RJ_ID = 'njsuxksdpaxjg9rhs6hxjnjvusda57fp'
+RJ_SECRET = 'yNrWJMaHF6pam9wj4TEpJcEuGE6dZ49Q3UujBhfRFD3'
+
+ERIC_ID = '4frhp5dwf7jnze6dhxdj39ujhdbkygft'
+ERIC_SECRET = 'DCceK33p3pc5YhpgYTBxGqFaax6ENHufsb4pKPMksj7'
+
 #ofile_stdout = open('stdout.txt', 'w')
 #ofile_stderr = open('stderr.txt', 'w')
 
 #os.dup2(ofile_stdout.fileno(), sys.stdout.fileno())
 #os.dup2(ofile_stderr.fileno(), sys.stderr.fileno())
 
-IDs = [0,0,0]
+IDs = [0,0,0,0,0,0,0,0]
 
 t1 = threading.Thread(target=thread_proc, args=(lock, CHRIS_ID, CHRIS_SECRET, 0, IDs))
 t1.start()
@@ -69,9 +84,24 @@ t2 = threading.Thread(target=thread_proc, args=(lock, CAMILLE_ID, CAMILLE_SECRET
 t2.start()
 t3 = threading.Thread(target=thread_proc, args=(lock, JOSH_ID, JOSH_SECRET, 2, IDs))
 t3.start()
+t4 = threading.Thread(target=thread_proc, argc=(lock, DAVID_ID, DAVID_SECRET, 3, IDs))
+t4.start()
+t5 = threading.Thread(target=thread_proc, args=(lock, AUDRIS_ID, AUDRIS_SECRET, 4, IDs))
+t5.start()
+t6 = threading.Thread(target=thread_proc, args=(lock, JOHN_ID, JOHN_SECRET, 5, IDs))
+t6.start()
+t7 = threading.Thread(target=thread_proc, args=(lock, RJ_ID, RJ_SECRET, 6, IDs))
+t7.start()
+t8 = threading.Thread(target=thread_proc, args=(lock, ERIC_ID, ERIC_SECRET, 7, IDs))
+t8.start()
 
 t1.join()
 t2.join()
 t3.join()
+t4.join()
+t5.join()
+t6.join()
+t7.join()
+t8.join()
 
 print 'Done with master thread'
