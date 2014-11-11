ac730beb704720b2690ef3e6110ccab74bea6ad5
Chris Daffron <cdaffron@vols.utk.edu>
2014-11-11 13:26:02 -0500

    Started work on getting workout data

---
+++ debug3/fitness_api.py
@@ -0,0 +1,171 @@
+
+import requests
+import time as time2
+import math
+import thread
+from datetime import datetime, date, time, timedelta
+
+class FitnessApi:
+  def __init__(self, client_id, client_secret):
+    self.client_id = client_id
+    self.client_secret = client_secret
+
+    self.calls = 0
+    self.calls_second = 0
+    self.last_time = time2.time()
+    self.limit = 24500
+    self.sec_limit = 55
+
+    self.access_token = self.get_access_token()
+
+  def increment_calls( self ):
+    self.calls += 1
+    self.calls_second += 1
+
+    # If over daily API call limit
+    if self.calls > self.limit:
+      print 'Reached API call limit. Waiting until next refresh time.'
+      self.wait()
+
+      print 'Refreshed. Grabbing more users.'
+      self.calls = 0
+      self.calls_second = 0
+
+    # If over seconds API call limit
+    elif self.calls_second > self.sec_limit:
+      print 'Reached API calls/sec limit. Waiting until next second.'
+      self.sec_wait()
+
+      print 'Refreshed. Grabbing more users.'
+      # self.calls = 0
+      self.calls_second = 0
+    elif time2.time >= self.last_time + 1:
+      self.last_time = time2.time()
+      self.calls_second = 0
+    return
+
+  def request( self, request_url ):
+    try:
+      response = requests.get(url=request_url, verify=False, headers={
+                  'api-key': self.client_id, 'authorization': 'Bearer %s' % self.access_token['access_token']})
+    except:
+      print 'API request failed (not error code)'
+      return ''
+
+    print 'Headers:'
+    print response.headers
+    
+    # Over API call limit
+    #if( response.status_code == 403 ):
+    #  self.calls = self.limit
+
+    self.increment_calls()
+
+    # Print debug info if the call fails
+    if( response.status_code != 200 ):
+      print 'ERROR! Received a status code of ' + str(response.status_code) + '\n'
+      print 'URL: ' + str(request_url) + '\n'
+      print 'Received Content:'
+      print response.content
+
+    try:
+      return response.json()
+    except:
+      return ''
+
+  def get_user_doc( self, userId ):
+    request_url = 'https://oauth2-api.mapmyapi.com/v7.0/user/' + str(userId) + '/'
+    user_doc = self.request(request_url)
+    return user_doc
+
+  def get_friends_with_doc( self, userId ): 
+    friends_with_doc = { 'userId' : userId, 'friends' : [] }
+
+    next_url = 'https://oauth2-api.mapmyapi.com/v7.0/user/?limit=20&friends_with=' + str( userId ) + '&offset=0'
+    while next_url:
+      friends_with_response = self.request( next_url )
+      
+      friends = friends_with_response['_embedded']['user']
+      for friend in friends:
+        user = { 'userId' : friend['id'] }
+        friends_with_doc['friends'].append( user )
+
+      try:
+        next_url = friends_with_response['_links']['next']['href']
+      except:
+        next_url = ''
+
+    return friends_with_doc
+
+  def get_workouts_doc( self, userId ):
+    workouts_doc = { 'userId' : userId, 'workouts': [] }
+    request_url = 'https://oauth2-api.mapmyapi.com/v7.0/workout/?user=%2Fv7.0%2Fuser%2F' + str(userId) + '%2F'
+    while request_url:
+      workouts_reponse = self.request( request_url )
+      workouts = workouts_reponse['_embedded']['workouts']
+      for workout in workouts:
+        print '\nWorkout'
+        print workout
+      request_url = ''
+    return ''
+
+  def get_access_token( self ):
+    access_token_url = 'https://api.mapmyfitness.com/v7.0/oauth2/access_token/'
+    access_token_data = {'grant_type': 'client_credentials',
+              'client_id': self.client_id,
+              'client_secret': self.client_secret}
+    try:
+      response = requests.post(url=access_token_url, data=access_token_data,
+                  headers={'Api-Key': self.client_id})
+    except:
+      print 'Request for access token failed (not error code)'
+      thread.exit()
+      
+    self.increment_calls()
+
+    # Print out debug info if the call is not successful
+    if( response.status_code != 200 ):
+      print 'ERROR! Received a status code of ' + str(response.status_code) + '\n'
+      print 'URL: ' + str(access_token_url) + '\n'
+      print 'Data: ' + str(access_token_data) + '\n'
+      print 'Received Content:'
+      print response.content
+      thread.exit()
+
+    try:
+      access_token = response.json()
+    except:
+      print 'Did not get JSON. Here is the response and content:'
+      print response 
+      print response.content 
+      access_token = ''
+
+    return access_token
+
+  
+
+  def wait( self ):
+    # Refresh at 12:30 AM GMT the next day
+    now = datetime.utcnow()
+    next_day = timedelta(days=1)
+    next_refresh_date = now.date() + next_day
+    next_refresh_time = time(0, 30)
+    next_refresh = datetime.combine( next_refresh_date, next_refresh_time )
+
+    # Wait until the next refresh period
+    while datetime.utcnow() < next_refresh:
+      difference = next_refresh - datetime.utcnow()
+      time2.sleep(difference.total_seconds())
+
+    return
+  
+  def sec_wait(self):
+    # Wait until the next second
+    next_sec = self.last_time + 1
+    while time2.time() < next_sec:
+      time2.sleep( abs(next_sec - time2.time()))
+
+    self.last_time = time2.time()
+
+    return
+
---
+++ debug3/fitness_db.py
@@ -0,0 +1,104 @@
+
+import pymongo
+
+
+class FitnessDatabase:
+
+  def __init__(self, host_name):
+    self.client = pymongo.MongoClient(host_name)
+    self.fitnessDb = self.client['MapMyFitness']
+
+    self.users = self.fitnessDb['users']
+    self.users_to_add = self.fitnessDb['users_to_add_copy2']
+    self.friends_with = self.fitnessDb['friends_with']
+
+  def users_insert(self, user_doc):
+    userId = user_doc['id']
+    ret = self.users.find_one({'id': userId})
+    not_already_added = (ret == None)
+
+    if not_already_added:
+      print 'Adding ' + str(userId) + ' to users'
+
+      try:
+        self.users.insert(user_doc)
+      except:
+        print 'Error: Unable to add ' + str(userId) + ' to users'
+
+    else:
+      print 'Not adding ' + str(userId) + ' to users'
+
+  def friends_with_insert(self, friends_with_doc):
+    userId = friends_with_doc['userId']
+    ret = self.friends_with.find_one({'userId': userId})
+    not_already_added = (ret == None)
+
+    if not_already_added:
+      print 'Adding ' + str(userId) + ' to friends_with'
+
+      try:
+        self.friends_with.insert(friends_with_doc)
+      except:
+        print 'Error: Unable to add ' + str(userId) + ' to friends_with'
+
+    else:
+      print 'Not adding ' + str(userId) + ' to friends_with'
+
+  def users_to_add_insert(self, user_doc):
+    userId = user_doc['userId']
+    ret1 = self.users.find_one({'id': userId})
+    ret2 = self.users_to_add.find_one({'userId': userId})
+
+    not_already_added = (ret1 == None) and (ret2 == None)
+    if not_already_added:
+      print 'Adding ' + str(userId) + ' to users_to_add'
+
+      try:
+        self.users_to_add.insert(user_doc)
+      except:
+        print 'Error: Unable to add ' + str(userId) + ' to users_to_add'
+
+    else:
+      print 'Not adding ' + str(userId) + ' to users_to_add'
+
+  def users_to_add_remove(self, user_doc):
+    self.users_to_add.remove(user_doc)
+
+  def get_num_users_to_add(self):
+    return self.users_to_add.count()
+
+  def users_to_add_empty(self):
+    if self.get_num_users_to_add() == 0:
+      return True
+    return False
+
+  def get_user_to_add(self):
+    return self.users_to_add.find_one()
+
+  def get_user(self):
+    return self.users.find_one()
+
+  def get_unique_user_to_add(self, thread_id, user_array):
+    if( thread_id == 0    ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 1  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 2  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 3  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 4  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 5  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 6  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 7  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 8  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[9] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 9  ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[10] } } ] } )
+    elif( thread_id == 10 ):
+      return self.users_to_add.find_one( { '$and': [ { 'userId': { '$ne': user_array[0] } }, { 'userId': { '$ne': user_array[1] } }, { 'userId': { '$ne': user_array[2] } }, { 'userId': { '$ne': user_array[3] } }, { 'userId': { '$ne': user_array[4] } }, { 'userId': { '$ne': user_array[5] } }, { 'userId': { '$ne': user_array[6] } }, { 'userId': { '$ne': user_array[7] } }, { 'userId': { '$ne': user_array[8] } }, { 'userId': { '$ne': user_array[9]  } } ] } )
+
